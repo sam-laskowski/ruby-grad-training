acb9b6951706dd7813b500adea0ff65ae2e7354d
Sam Laskowski <sam.laskowski@outlook.com>
2026-03-18 17:00:23 +0000

    add require login method

--- app/controllers/concerns/authenticated.rb
+++ app/controllers/concerns/authenticated.rb
@@ -17,4 +17,11 @@ module Authenticated
     current_user.present?
   end
 
+  def require_login
+    unless user_signed_in?
+      flash[:alert] = "You must be signed in to access this section"
+      redirect_to root_path # Or your login_path
+    end
+  end
+
 end
