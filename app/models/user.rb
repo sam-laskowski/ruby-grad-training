cc23b1a2b84cd152387932a916cb1d46486f8202
Sam Laskowski <sam.laskowski@outlook.com>
2026-03-19 13:19:43 +0000

    KAN-21 add configurable image sizes and dependencies

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
   has_many :owned_games, class_name: "GamePost", foreign_key: "user_owner_id"
   has_many :enrollments
   has_many :game_posts, through: :enrollments
+
+  def photo_as_thumbnail
+    photo.variant(resize_to_limit: [300, 300]).processed
+  end
 end
