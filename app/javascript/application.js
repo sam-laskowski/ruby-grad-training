c2985b135cdc3ca0fe7c8e7c085ace3c8be2cd28
Sam Laskowski <117819316+samelpo@users.noreply.github.com>
2026-03-23 16:50:23 +0000

    toasties 🥪

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,4 +5,32 @@ import "controllers";
 import "trix";
 import "@rails/actiontext";
 
-import "bootstrap";
+import * as bootstrap from "bootstrap";
+
+const initToasts = () => {
+  document.querySelectorAll(".toast").forEach((toastEl) => {
+    const toast = bootstrap.Toast.getOrCreateInstance(toastEl, {
+      delay: 3000,
+      autohide: true,
+    });
+
+    if (toastEl.id === "liveToast") return;
+    toast.show();
+  });
+
+  const toastTrigger = document.getElementById("liveToastBtn");
+  const toastLiveExample = document.getElementById("liveToast");
+
+  if (toastTrigger && toastLiveExample) {
+    toastTrigger.onclick = () => {
+      const toast = bootstrap.Toast.getOrCreateInstance(toastLiveExample, {
+        delay: 3000,
+        autohide: true,
+      });
+      toast.show();
+    };
+  }
+};
+
+document.addEventListener("DOMContentLoaded", initToasts);
+document.addEventListener("turbo:load", initToasts);
